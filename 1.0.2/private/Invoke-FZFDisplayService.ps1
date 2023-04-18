791ebff38705637b0eaee05be9614bad3eca598e
Joshua Ablian Saputil <joshuasaputil97@hotmail.com>
2023-04-18 12:34:46 +0200

    Developing

--- 1.0.2/private/Invoke-FZFDisplayService.ps1
+++ 1.0.2/private/Invoke-FZFDisplayService.ps1
@@ -1,7 +1,8 @@
 function Invoke-FZFDisplayService {
+  [CmdletBinding()]
   param (
     [Parameter()]
-    [Object]
+    [Object[]]
     $Object
     ,
     [Parameter()]
@@ -9,36 +10,44 @@ function Invoke-FZFDisplayService {
     $SearchProperties
   )
   
-  $Properties = ($Object | Get-Member -MemberType Properties).Name
+  $Properties = ($Object[0] | Get-Member -MemberType Properties).Name
   if ($PSBoundParameters["SearchProperties"] -is [System.Object]) {
     $ValidProperties = foreach ($SearchProperty in $SearchProperties) {
       if ($SearchProperty -in $Properties) {
-        $SearchProperty 
-      } 
-    } 
-  } 
+        $SearchProperty  
+      }
+    }
+  }
   else 
   { 
-    $ValidProperties = $Properties | Invoke-PropertiesTelescope 
+    $ValidProperties = $Object[0] | Invoke-PropertiesTelescope
   }
   
   $TempFolder = Invoke-RandTempFolderGeneration -GenerateTempFolder
-  Write-Host $TempFolder
-  $Hashtable = [Hashtable]::new()
+  $DataHT = [Hashtable]::new()
+  $ReturnHT = [hashtable]::new()
   
   foreach ($ObjectItem in $Object) {
     $List = [System.Collections.ArrayList]::new()
+    
     $FileName = foreach ($Vproperties in $ValidProperties) {
       $ObjectItem.$Vproperties
     }
+    
     $FileName = $FileName -join ""
     $ItemURL = "$TempFolder\$($FileName)___.json"
+    
     foreach ($Property in $Properties) {
-      $List.Add(@{$Property = $ObjectItem.$Property})
+      [void] $List.Add(@{$Property = $ObjectItem.$Property}) 
     }
-    $Hashtable.Add($FileName, $List)
-    $Hashtable | ConvertTo-Json >> $ItemURL
+    
+    $DataHT.Add($FileName, $List)
+    @{$FileName = $DataHT["$FileName"]} | ConvertTo-Json >> $ItemURL
   }
   
-  return $Hashtable
+  $ReturnHT.Add("TempFolderPath", $TempFolder)
+  $ReturnHT.Add("DataHashtable", $DataHT)
+  $ReturnHT.Add("PickerList", $DataHT.Keys)
+  
+  return $ReturnHT
 } 
