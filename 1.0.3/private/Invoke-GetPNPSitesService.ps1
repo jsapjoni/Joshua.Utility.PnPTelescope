61d15947774295f09df9588d20a73a017545423a
Joshua Ablian Saputil <joshuasaputil97@hotmail.com>
2023-04-19 07:58:58 +0200

    fixes on Invoke-GetPnPSitesServer

--- 1.0.3/private/Invoke-GetPNPSitesService.ps1
+++ 1.0.3/private/Invoke-GetPNPSitesService.ps1
@@ -2,10 +2,12 @@ function Invoke-GetPNPSitesService {
   try 
   {
     $Sites = Get-PnPTenantSite -Detailed -IncludeOneDriveSites
+    return $Sites
   }
   catch 
   {
-    Write-Host "Could not gather sites info from tenant using PnP module"
+    Write-Host "Could not gather sites info from tenant using " -NoNewline
+    Write-Host "$((Get-Command -Name "Get-PnPTenantSite").Source)" -ForegroundColor Red 
     Write-Host "Attempting to use SPO management module to gather sites info"
   }
   
@@ -14,10 +16,13 @@ function Invoke-GetPNPSitesService {
     $AdminURL = (Get-PnPContext).Url | Invoke-SPOAdminURLConverter
     Connect-SPOService -Url $AdminURL -ModernAuth $true
     $Sites = Get-SPOSite -Limit ALL -IncludePersonalSite $true
+    return $Sites
   }
   catch 
   {
-    throw "Could not gather sites info from both PnP Module and SPO Management Module"
+    Write-Host "Could not gather sites info from tenant using " -NoNewline
+    Write-Host "$((Get-Command -Name "Connect-SPOService").Source)" -ForegroundColor Red 
+    Write-Host "Aborting function call..." -ForegroundColor Red
+    throw 
   }
-  return $Sites
 }
